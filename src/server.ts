6777d7cfba5f20bc7f32e99ed168c495624693a1
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-11-06 09:22:01 -0300

    config swagger

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 import { routes } from "./routes";
 import * as dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 routes(app);
 
